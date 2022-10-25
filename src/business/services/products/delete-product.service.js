56c072a648e3cc70fb822950cac875eb239cfdac
Flores Roman <diego.a.flores.roman@accenture.com>
2022-10-25 14:45:48 -0300

    deploy

--- src/business/services/products/delete-product.service.js
+++ src/business/services/products/delete-product.service.js
@@ -2,22 +2,20 @@
 const productRepository = require('../../../data/repository/products.repository')
 
 const execute = async (id) => {
-	try {
-		const productData = await deleteProduct(parseInt(id))
-		const product = productConverter(productData)
-		return product
-	} catch (error) {
-		throw error
-	}
+  try {
+    await deleteProduct(parseInt(id))
+  } catch (error) {
+    throw error
+  }
 }
 
 const deleteProduct = async (id) => {
-	try {
-		const result = await productRepository.deleteProduct(id)
-		return result
-	} catch (error) {
-		throw { httpCode: 422, message: error.message }
-	}
+  try {
+    const result = await productRepository.deleteProduct(id)
+    return result
+  } catch (error) {
+    throw { httpCode: 422, message: error.message }
+  }
 }
 
 module.exports = { execute }
